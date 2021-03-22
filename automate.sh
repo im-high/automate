bb04cb77705fa5d5bb0421a4ffb9c2c8a7409136
im-high <74088194+im-high@users.noreply.github.com>
2021-03-22 03:24:06 -0400

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -5,6 +5,7 @@ pkg install python -y
 pkg install python2 -y
 pkg install git -y
 pip install lolcat
+cd ~
 git clone https://github.com/noob-hackers/grabcam
 pkg install php -y
 pkg install wget -y
