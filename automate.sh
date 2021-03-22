af16cd413d5fa23b108c9aa976790bd76091a730
im-high <74088194+im-high@users.noreply.github.com>
2021-03-22 03:03:08 -0400

    Add files via upload

---
+++ automate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+apt-get update -y
+apt-get upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pip install lolcat
+git clone https://github.com/noob-hackers/grabcam
+pkg install php -y
+pkg install wget -y
